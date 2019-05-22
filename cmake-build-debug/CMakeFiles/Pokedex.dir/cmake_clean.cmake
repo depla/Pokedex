1c57e4a58685b0ceb2f34447b387311a0b402e8c
Dennis La <dennisla95@yahoo.com>
2019-05-21 18:53:59 -0700

    update stuff

---
+++ cmake-build-debug/CMakeFiles/Pokedex.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pokedex.dir/main.cpp.o"
+  "CMakeFiles/Pokedex.dir/Pokemon.cpp.o"
+  "CMakeFiles/Pokedex.dir/Pokedex.cpp.o"
+  "CMakeFiles/Pokedex.dir/PokemonImage.cpp.o"
+  "Pokedex.pdb"
+  "Pokedex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pokedex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
